8f96ba341a2f5ab6735c0fe591f0da31e2014707
phj <parkhj8801@gmail.com>
2019-10-09 02:03:45 +0900

    html,css,js추가

--- js/main.js
+++ js/main.js
@@ -63,13 +63,118 @@ $(function(){
               min: 0,
               max: 100,
               values: [ 10, 500 ],
-              slide: function( event, ui ) {
+              create: function( event, ui ) {
+                var slideVal = $('.price_filter .price_range span');
+                var slideFirstVal = slideVal.filter(':first-of-type');
+                var slideLastVal = slideVal.filter(':last-of-type');
+                slideVal.html('<i></i>');
+
+                slideFirstVal.find('i').text($(".price_range").slider("values",0)+'$');
+                slideLastVal.find('i').text($(".price_range").slider("values",1)+'$');
+
+              },
+              slide: function( event, ui ) { //값이 바뀔때 마다 할일
+                var slideVal = $('.price_filter .price_range span');
+                var slideFirstVal = slideVal.filter(':first-of-type');
+                var slideLastVal = slideVal.filter(':last-of-type');
+                slideVal.html('<i></i>');
+
                 $( "#from" ).val( "$" + ui.values[ 0 ] );
                 $("#to").val("$" + ui.values[ 1 ] );
+                slideFirstVal.find('i').text(ui.values[0]+'$');
+                slideLastVal.find('i').text(ui.values[1]+'$');
               }
             });
             $('#from').val($(".price_range").slider("values",0));
             $('#to').val($(".price_range").slider("values",1));
+
+            $('#from').change(function(){
+                var userValue = $(this).val();
+                $(".price_range").slider("value", 0, userValue);  
+                
+                var slideVal = $('.price_filter .price_range span');
+                var slideFirstVal = slideVal.filter(':first-of-type');
+            
+                slideFirstVal.html('<i></i>');
+                slideFirstVal.find('i').text(userValue +'$');
+            });
+            $('#to').change(function(){
+                var userValue = $(this).val();
+                var slideVal = $('.price_filter .price_range span');
+                var slideLastVal = slideVal.filter(':last-of-type');
+
+                $(".price_range").slider("value", 1, userValue);
+                slideLastVal.html('<i></i>');
+                slideLastVal.find('i').text(userValue +'$');
+            });             
         });
+
+        /* 제이쿼리 필터구현
+        var sizeFilter = $('.size_filter input');
+        var targetList = $('.new_arrivals_list li');
+
+        sizeFilter.click(function(){
+            var targetValue = [];
+            //input:checked
+            sizeFilter.filter(':checked').each(function(){
+                targetValue.push('.'+$(this).val());
+            });//each
+
+            //기존배열의 요소들 사이사이에 ', '를 넣어서 하나로 합쳐서 반환
+            var targetClass=targetValue.join(', '); 
+            targetList.hide();  //기존의 상품목록(li)들은 가려주고
+            $(targetClass).show(); //선택한상품만 보여지게
+                  
+        });//sizeFilter.click
+        */
+
+        /* 플러그인 필터구현 */
+        // init Isotope
+        var $filters = $('.combi_filters input');
+        var filters = {};
+        var $grid = $('.new_arrivals_list').isotope({
+            itemSelector:'.new_arrivals_list > li'
+        });
+
+        $filters.click(function(){
+            var $button = $(this);
+
+            //get group key
+            var $buttonGroup = $button.parent('div');
+            var filterGroup = $buttonGroup.attr('data-filter-group');
+
+            //set filter for group
+            filters[filterGroup] = $button.val();
+
+            //combine filters
+            var filterValue = concatValues(filters);
+
+            //set filter for Isotope
+            $grid.isotope({filter: filterValue});
+
+            //all을 클릭했을때 나머지는 uncheck
+            if($button.val() == '*'){  //클릭한버튼의 value값이 *일때
+                
+                $button.parent('div').find('input').prop('checked',false);  //$filters요소들의 check를 전부 풀고
+                $button.prop('checked',true); //클릭한요소만 check되도록 
+            } else{
+                $button.parent('div').find('input').eq(0).prop('checked',false);
+            }
+
+        });//filter click
+
+        // flatten object by concatting values
+        function concatValues( obj ) { //obj로 배열이 넘어온다.
+            var value = '';
+            for ( var prop in obj ) {  
+            value += obj[ prop ];
+            }
+            
+            console.log(value);
+            return value;
+        }
+        
+        //페이지 열자마자 모든 상품리스트가 보여지게
+        $grid.isotope({ filter: '*' })
 
 }); //document ready function
